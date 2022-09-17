44e18b3bb5dd975d86c2873d28b528dbed51fd06
miwanoff <marina.iva.noffa@gmail.com>
2022-09-17 15:16:00 +0300

    Change temp.php

--- temp.php
+++ temp.php
@@ -1,9 +1,12 @@
 <?php
-$x = 0;
+$x = rand(-300, 500) / 10;
+$x = (int) $x;
 if ($x < 0) {
-    echo "Мороз";
-} elseif ($x > 0) {
-    echo "Тепло";
-} else {
-    echo "!";
+    echo "<p style = \" color: blue;\">Мороз! Температура: </p>" . $x;
+}
+if ($x == 0) {
+    echo "<p style = \" color: orange;\">Не мороз и не тепло. Температура: </p>" . $x;
+}
+if ($x > 0) {
+    echo "<p style = \" color: red;\">Тепло! Температура: </p>" . $x;
 }
