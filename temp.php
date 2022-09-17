3355514cc49f36dcd0bd95be25b3ed2494e4e400
miwanoff <marina.iva.noffa@gmail.com>
2022-09-17 15:23:30 +0300

    Change color

--- temp.php
+++ temp.php
@@ -2,7 +2,7 @@
 $x = rand(-300, 500) / 10;
 $x = (int) $x;
 if ($x < 0) {
-    echo "<p style = \" color: blue;\">Мороз! Температура: </p>" . $x;
+    echo "<p style = \" color: red;\">Мороз! Температура: </p>" . $x;
 }
 if ($x == 0) {
     echo "<p style = \" color: orange;\">Не мороз и не тепло. Температура: </p>" . $x;
